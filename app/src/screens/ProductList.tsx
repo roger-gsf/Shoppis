b2b6a2bb30fd11a9575351166801b260736529c3
MatheusHrique <notch02005@gmail.com>
2024-11-07 21:17:44 -0300

    :bug: fix: Lista de produtos funcionando

--- app/src/screens/ProductList.tsx
+++ app/src/screens/ProductList.tsx
@@ -18,9 +18,9 @@ const ProductList = () => {
   const [produtos, setProdutos] = useState<ProductDTO[]>();
   const { cart, getCart, addProduct, removeProduct } = useContext(CartContext);
 
-  // useEffect(() => {
-  //   getData("");
-  // });
+  useEffect(() => {
+    getData();
+  });
 
   // <View>
   //     <Text>Lo Carto!!!</Text>
@@ -43,7 +43,6 @@ const ProductList = () => {
     try {
       const response = await axiosInstance.get<ProductDTO[]>("/products");
       setProdutos(response.data);
-      console.log(response.data);
     } catch (error) {
       console.log("Erro ao fazer fetching data:", error);
     }
@@ -53,14 +52,12 @@ const ProductList = () => {
     <SafeAreaProvider>
       <ScrollView showsVerticalScrollIndicator={true} style={styles.scrollView}>
         <SafeAreaView>
-          <TouchableOpacity onPress={() => getData()}>
-            <Text>Atualizar produtos</Text>
-          </TouchableOpacity>
-          <Text>ProductList</Text>
           <FlatList
-            style={{ alignSelf: "center", flex: 1 }}
+            style={{
+              marginLeft: "4.5%",
+            }}
             data={produtos}
-            renderItem={ProductCard}
+            renderItem={({ item }) => <ProductCard item={item} />}
             keyExtractor={(item) => item.id.toString()}
             contentContainerStyle={{
               flexGrow: 1,
