c8ea0082e49898e1b9dc848e5ff85e9dfb56181a
04374771000 <04374771000@senacrs.edu.br>
2024-11-07 21:46:54 -0300

    :lipstick: feat: updating userDetails styles

--- app/src/screens/UserDetails.tsx
+++ app/src/screens/UserDetails.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { useUserContext } from "../contexts/UserContext";
 
@@ -6,22 +6,55 @@ const UserDetails = () => {
   const { userData, handleLogout } = useUserContext();
 
   return (
-    <View>
+    <View style={styles.container}>
+      {/* Standard avatar:
       <Image
-        style={{ width: 100, height: 100 }}
+        style={{ width: 150, height: 150 }}
         source={{
           uri: userData?.user.avatar,
         }}
+      /> 
+      */}
+
+      <Image
+        style={styles.image}
+        source={require('../assets/ching-ling.png')}
       />
+
       <Text>
         {userData?.user.fname ?? ""} {userData?.user.lname ?? ""}
       </Text>
       <Text>{userData?.user.email ?? ""}</Text>
-      <Button onPress={handleLogout} title="Sair" />
+      <TouchableOpacity onPress={handleLogout} style={styles.exitButton}>
+        <Text style={styles.buttonText}>Sair</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default UserDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 8
+  },
+  image: {
+    width: 150, 
+    height: 150,
+    borderRadius: 20
+  },
+  exitButton: {
+    justifyContent: 'center',
+    width: 145,
+    height: 38,
+    backgroundColor: 'gray',
+    borderRadius: 8
+  },
+  buttonText: {
+    textAlign: 'center',
+  }
+});
