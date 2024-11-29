f62f094883c3e2f210fe68da74664a9d69ca78b3
Róger Ferreira <rogerferreira610@gmail.com>
2024-11-29 00:24:10 -0300

    :bug: fixing some details

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,8 @@
-import { RootSiblingParent } from "react-native-root-siblings";
-import Routes from "./src/routes";
 import React from "react";
+import Routes from "./src/routes";
+import { RootSiblingParent } from "react-native-root-siblings";
 import { UserProvider } from "./src/contexts/UserContext";
 import { CartContextProvider } from "./src/contexts/CartContext";
-import PaymentInfo from "./src/screens/PaymentInfo";
-
 
 export default function App() {
   return (
